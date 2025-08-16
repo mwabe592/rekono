2d5f646a62c99ea85c67e85dfce1d2b587f68eb6
Ben Mwangi <benmwangi123@gmail.com>
2025-08-16 14:38:25 +1200

    posthog analytics setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/Providers";
 import ToasterContext from "./context/ToastContext";
+import { PostHogProvider } from "@/components/PostHogProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,10 +21,13 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`dark:bg-black ${inter.className}`}>
+        <PostHogProvider>
+
         <Providers>
           <ToasterContext />
           {children}
         </Providers>
+        </PostHogProvider>
       </body>
     </html>
   );
